868447f8adbc17c3fdc838487ab46c74df8c2815
Matthew Pryce <lsh1800165@lshtm.ac.uk>
2024-12-03 18:17:32 +0000

    Adding in LT options

--- scripts/Data_gen_surv.R
+++ scripts/Data_gen_surv.R
@@ -35,13 +35,21 @@ data_gen_surv <- function(n = 1000,
                           x = 10,
                           x_form = c("Unif","Norm"),
                           e_func,
-                          Y_type,
-                          H_0_lambda_Y,
-                          H_0_gamma_Y,
-                          cox_func_Y,
-                          aft_func_Y,
-                          pois_func_Y,
-                          unif_end_Y,
+                          T_type,
+                          H_0_lambda_T,
+                          H_0_gamma_T,
+                          cox_func_T,
+                          aft_func_T,
+                          pois_func_T,
+                          unif_end_T,
+                          Q_type,
+                          H_0_lambda_Q,
+                          H_0_gamma_Q,
+                          cox_func_Q,
+                          aft_func_Q,
+                          pois_func_Q,
+                          unif_end_Q,
+                          max_Q = NULL,
                           C_type,
                           H_0_lambda_C,
                           H_0_gamma_C,
@@ -49,7 +57,8 @@ data_gen_surv <- function(n = 1000,
                           aft_func_C,
                           pois_func_C,
                           unif_end_C,
-                          right_cen = NULL)
+                          right_cen = NULL,
+                          LT = NULL)
   {
   ID <- rep(1:n)
   
@@ -80,105 +89,192 @@ data_gen_surv <- function(n = 1000,
   A <- rbinom(n, size=1, prob= prop_score)
   X <- cbind(X,A)
   
-  if (Y_type == "Cox"){
+  if (T_type == "Cox"){
     #Cox needs to specify baseline hazard and combination
     U <- runif(n)
-    T <- (-log(U) / (H_0_lambda_Y * exp(cox_func_Y(X))))^(1 / H_0_gamma_Y)
+    evt_T <- (-log(U) / (H_0_lambda_T * exp(cox_func_T(X))))^(1 / H_0_gamma_T)
   }
-  if (Y_type == "AFT"){
+  if (T_type == "AFT"){
     epsilon <- rnorm(dim(X)[1])
-    log_T <- aft_func_Y(X) + epsilon
-    T <- exp(log_T)
+    log_T <- aft_func_T(X) + epsilon
+    evt_T <- exp(log_T)
   }
-  if (Y_type == "Poisson"){
-    T <- rep(-99,n)
-    event_counts <- rpois(n, lambda = pois_func_Y(X))
+  if (T_type == "Poisson"){
+    evt_T <- rep(-99,n)
+    event_counts <- rpois(n, lambda = pois_func_T(X))
     for (i in 1:n){
       if (event_counts[i] > 0){
-        T[i] <- rexp(1, rate = event_counts[i])
+        evt_T[i] <- rexp(1, rate = event_counts[i])
       }
       else {
-        T[i] <- Inf
+        evt_T[i] <- Inf
       }
     }
   }
-  if (Y_type == "Unif"){
-    T <- runif(n,0,unif_end_Y)
+  if (T_type == "Unif"){
+    evt_T <- runif(n,0,unif_end_T)
   }
-
-  if (C_type == "Cox"){
-    #Cox needs to specify baseline hazard and combination
-    U <- runif(n)
-    C <- (-log(U) / (H_0_lambda_C * exp(cox_func_C(X))))^(1 / H_0_gamma_C)
-  }
-  if (C_type == "AFT"){
-    epsilon <- rnorm(dim(X)[1])
-    log_C <- aft_func_C(X) + epsilon
-    C <- exp(log_C)
-  }
-  if (C_type == "Poisson"){
-    C <- rep(-99,n)
-    cen_counts <- rpois(n, lambda = pois_func_C(X))
-    for (i in 1:n){
-      if (cen_counts[i] > 0){
-        C[i] <- rexp(1, rate = cen_counts[i])
-      }
-      else {
-        C[i] <- Inf
-      }
-    }
-  }
-  if (C_type == "Unif"){
-    C <- runif(n,0,unif_end_C)
-  }
-
-  #Determine observed times (minimum of survival and censoring times)
-  T_tilde <- pmin(T, C)
-
-  if (is.null(right_cen) == 0){
-    T_tilde <- pmin(T_tilde,right_cen)
-  }
-
-  # Indicator for censoring (1 if the event occurred, 0 if censored)
-  Y <- as.numeric((T <= C) & (T_tilde != right_cen))
-  delta <- as.numeric((T <= C))
-  cen_ind <- 1-Y
-
 
   
-  # #Collating dataset
-  sim_data <- data.frame(X,prop_score,T,C,T_tilde,Y,delta,cen_ind)
+  if (is.null(LT)==1){
+    if (C_type == "Cox"){
+      #Cox needs to specify baseline hazard and combination
+      U <- runif(n)
+      C <- (-log(U) / (H_0_lambda_C * exp(cox_func_C(X))))^(1 / H_0_gamma_C)
+    }
+    if (C_type == "AFT"){
+      epsilon <- rnorm(dim(X)[1])
+      log_C <- aft_func_C(X) + epsilon
+      C <- exp(log_C)
+    }
+    if (C_type == "Poisson"){
+      C <- rep(-99,n)
+      cen_counts <- rpois(n, lambda = pois_func_C(X))
+      for (i in 1:n){
+        if (cen_counts[i] > 0){
+          C[i] <- rexp(1, rate = cen_counts[i])
+        }
+        else {
+          C[i] <- Inf
+        }
+      }
+    }
+    if (C_type == "Unif"){
+      C <- runif(n,0,unif_end_C)
+    }
+    
+    #Determine observed times (minimum of survival and censoring times)
+    T_tilde <- pmin(T, C)
+    
+    if (is.null(right_cen) == 0){
+      T_tilde <- pmin(T_tilde,right_cen)
+    }
+    
+    # Indicator for censoring (1 if the event occurred, 0 if censored)
+    delta <- as.numeric((evt_T <= C))
+    cen_ind <- 1-delta
+  }
+  
+  if (LT == 1){
+    #--- Generating truncation time Q ---#
+    if (Q_type == "Cox"){
+      #Cox needs to specify baseline hazard and combination
+      U <- runif(n)
+      Q <- (-log(U) / (H_0_lambda_Q * exp(cox_func_Q(X))))^(1 / H_0_gamma_Q)
+    }
+    if (Q_type == "AFT"){
+      epsilon <- rnorm(dim(X)[1])
+      log_Q <- aft_func_Q(X) + epsilon
+      Q <- exp(log_Q)
+    }
+    if (Q_type == "Poisson"){
+      Q <- rep(-99,n)
+      event_counts <- rpois(n, lambda = pois_func_Q(X))
+      for (i in 1:n){
+        if (event_counts[i] > 0){
+          Q[i] <- rexp(1, rate = event_counts[i])
+        }
+        else {
+          Q[i] <- Inf
+        }
+      }
+    }
+    if (Q_type == "Unif"){
+      Q <- runif(n,0,unif_end_Q)
+    }
+    
+    #Adding in max trunc option 
+    if (is.null(max_Q)==1){
+      max_Q <- evt_T - 0.05
+      Q <- pmin(Q,max_Q)
+    }
+    if (is.null(max_Q) == 0){
+      Q <- pmin(Q,max_Q)
+    }
+    Q <- pmax(Q,0)
+    
+    
+    #--- Generating censoring time C=Q+D ---#
+    if (C_type == "Cox"){
+      #Cox needs to specify baseline hazard and combination
+      U <- runif(n)
+      D <- (-log(U) / (H_0_lambda_C * exp(cox_func_C(X))))^(1 / H_0_gamma_C)
+    }
+    if (C_type == "AFT"){
+      epsilon <- rnorm(dim(X)[1])
+      log_D <- aft_func_C(X) + epsilon
+      D <- exp(log_D)
+    }
+    if (C_type == "Poisson"){
+      D <- rep(-99,n)
+      cen_counts <- rpois(n, lambda = pois_func_C(X))
+      for (i in 1:n){
+        if (cen_counts[i] > 0){
+          D[i] <- rexp(1, rate = cen_counts[i])
+        }
+        else {
+          D[i] <- Inf
+        }
+      }
+    }
+    if (C_type == "Unif"){
+      D <- runif(n,0,unif_end_C)
+    }
+    
+    C <- Q + D
+    
+    #--- Generating observed time ---#
+    #Determine observed times (minimum of survival and censoring times)
+    T_tilde <- pmin(evt_T, C)
+    
+    if (is.null(right_cen) == 0){
+      T_tilde <- pmin(T_tilde,right_cen)
+    }
+    
+    # Indicator for censoring (1 if the event occurred, 0 if censored)
+    delta <- as.numeric((evt_T <= C))
+    cen_ind <- 1-delta
+    
+  }
+  
+  #Collating dataset
+  if (is.null(LT)==1){
+    sim_data <- data.frame(X,prop_score,T,C,T_tilde,delta,cen_ind)
+  }
+  else if (LT == 1){
+    sim_data <- data.frame(X,prop_score,T=evt_T,Q,D,C,T_tilde,delta,cen_ind)
+  }
   return(sim_data)
 }
 
 #####################################################################
 # 
 # e_func <- function(X){
-#   X2 <- X$X2; 
+#   X2 <- X$X2;
 #   X3 <- X$X3;
 #   e_test <- plogis(pmax(X2,X3))
 #   e_test
 # }
 # 
 # cox_func <- function(X){
-#   X2 <- X$X2; 
+#   X2 <- X$X2;
 #   X3 <- X$X3;
 #   cox_test <- pmax(X2,X3)
 #   cox_test
 # }
 # 
 # aft_func <- function(X){
-#   X2 <- X$X2; 
+#   X2 <- X$X2;
 #   X3 <- X$X3;
 #   epsilon <- rnorm(dim(X)[1])
-#   aft_test <- pmax(X2,X3) + epsilon 
+#   aft_test <- pmax(X2,X3) + epsilon
 #   aft_test
 # }
 # 
 # pois_func <- function(X){
-#   X2 <- X$X2; 
+#   X2 <- X$X2;
 #   X3 <- X$X3;
-#   pois_test <- X2+X3 
+#   pois_test <- X2+X3
 #   pois_test
 # }
 # 
@@ -194,13 +290,21 @@ data_gen_surv <- function(n = 1000,
 #                           x = covs,
 #                           x_form = "Unif",
 #                           e_func = e_func,
-#                           Y_type = "Poisson",
-#                           cox_func_Y = cox_func,
-#                           aft_func_Y = aft_func,
-#                           pois_func_Y = pois_func,
-#                           unif_end_Y = 3,
-#                           H_0_lambda_Y = H_0_lambda,
-#                           H_0_gamma_Y = H_0_gamma,
+#                           T_type = "AFT",
+#                           cox_func_T = cox_func,
+#                           aft_func_T = aft_func,
+#                           pois_func_T = pois_func,
+#                           unif_end_T = 3,
+#                           H_0_lambda_T = H_0_lambda,
+#                           H_0_gamma_T = H_0_gamma,
+#                           Q_type = "Cox",
+#                           cox_func_Q = cox_func,
+#                           aft_func_Q = aft_func,
+#                           pois_func_Q = pois_func,
+#                           unif_end_Q = 3,
+#                           H_0_lambda_Q = H_0_lambda,
+#                           H_0_gamma_Q = H_0_gamma,
+#                           # max_Q = 1,
 #                           C_type = "Unif",
 #                           cox_func_C = cox_func,
 #                           aft_func_C = aft_func,
@@ -208,7 +312,8 @@ data_gen_surv <- function(n = 1000,
 #                           H_0_lambda_C = H_0_lambda,
 #                           H_0_gamma_C = H_0_gamma,
 #                           unif_end_C = 3,
-#                           right_cen = 1)
+#                           right_cen = 5,
+#                           LT = 1)
 # 
 
 # #Finding truth 
